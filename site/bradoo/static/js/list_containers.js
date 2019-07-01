325c259b12dbcec85e217d22c90c5e4d6f77ccac
marbueno <marbue@ig.com.br>
2019-06-30 23:10:33 -0300

    Tarefas - Slide 3 e 4

--- site/bradoo/static/js/list_containers.js
+++ site/bradoo/static/js/list_containers.js
@@ -1,4 +1,5 @@
 var images = []
+var build = {}
 
 $("#createbuild").submit(function (event) {
     event.preventDefault();
@@ -37,74 +38,106 @@ $("#createbuild").submit(function (event) {
         html: 'Tempo de Execução <strong></strong> segundos.',
         timer: 15000,
         onBeforeOpen: () => {
-        Swal.showLoading()
-        timerInterval = setInterval(() => {
-          Swal.getContent().querySelector('strong')
-            .textContent = Swal.getTimerLeft()
-        }, 100)
+            Swal.showLoading()
+            timerInterval = setInterval(() => {
+            Swal.getContent().querySelector('strong')
+                .textContent = Swal.getTimerLeft()
+            }, 100)
         },
         onClose: () => {
         clearInterval(timerInterval)
         }
-        }).then((result) => {
-        if (
-            // Read more about handling dismissals
-            result.dismiss === Swal.DismissReason.timer
-            ) {
-                $('#outputJenkins').modal('show');
-            }
-        });
-    return false
 
+    }).then((result) => {
+        if (result.dismiss === Swal.DismissReason.timer) {
+            $('#outputJenkins').modal('show');
+        }
+    });
+    return false
+});
+
+
+$('#updatebuild').submit(function (event) {
+    event.preventDefault();
+    debugger;
+    var data = $( this ).serializeArray();
+    var url = 'http://18.219.63.233:5000/build/' + build._id + "/";
+
+    data.push({ name: "product", value: build.product});
+    data.push({ name: "image_tag", value: build.image_tag});
+    data.push({ name: "name", value: build.name});
+    
+    $.ajax({
+        type: "PUT",
+        url: url,
+        data :  JSON.stringify(data),
+        contentType: "application/json; charset=utf-8",
+        datatype: "json",
+        success:function () {
+            $('#updateJob').modal('hide');
+            $('body').removeClass('modal-open');
+            $('.modal-backdrop').remove();
+            Swal.fire({
+                type: 'success',
+                title: 'Sucesso!',
+                text: 'Build Executado!'
+            });
+
+        },
+        error:function (ex) {
+            console.log(ex);
+            Swal.fire({
+                type:'error',
+                title: 'Oops...',
+                text:'Falha ao executar o build!'
+            });
+
+        }
+    })
 });
 
 $('input[id^="switch"]').click(function (e) {
     e.preventDefault();
     var data = $( this ).val();
     Swal.fire({
-          title: 'Deseja scalar ?',
-          text: "A aplicação sofrera alterações!",
-          type: 'warning',
-          showCancelButton: true,
-          confirmButtonColor: '#3085d6',
-          cancelButtonColor: '#d33',
-          confirmButtonText: 'Sim, desejo escalar!'
-        }).then((result) => {
-          if (result.value) {
-              $.ajax({
-        type: 'POST',
-        url:'http://18.219.63.233:5000/scale/',
-        data: data,
-        datatype: 'text',
-        success:function () {
-            Swal.fire({
-                type: 'success',
-                title: 'Sucesso!',
-                text: 'Scale executado!',
-            }).then(function () {
-                window.location.href= '/deployments';
+        title: 'Deseja scalar ?',
+        text: "A aplicação sofrera alterações!",
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Sim, desejo escalar!'
+    }).then((result) => {
+        if (result.value) {
+            $.ajax({
+                type: 'POST',
+                url:'http://18.219.63.233:5000/scale/',
+                data: data,
+                datatype: 'text',
+                success:function () {
+                    Swal.fire({
+                        type: 'success',
+                        title: 'Sucesso!',
+                        text: 'Scale executado!',
+                    }).then(function () {
+                        window.location.href= '/deployments';
 
-            });
-        },
-        error:function () {
-            Swal.fire({
-                type:'error',
-                title: 'Oops...',
-                text:'Falha ao Scalar'
-            }).then(function () {
-                window.location.href= '/deployments';
+                    });
+                },
+                error:function () {
+                    Swal.fire({
+                        type:'error',
+                        title: 'Oops...',
+                        text:'Falha ao Scalar'
+                    }).then(function () {
+                        window.location.href= '/deployments';
 
+                    });
+                }
             });
         }
-
-    });
-          }
-        })
-
-
+    })
 });
-
-
 
 $("#auto_fill_product").focusout(function () {
     var product = $( this ).val();
@@ -124,41 +157,6 @@ $("#auto_fill_product").focusout(function () {
     })
 });
 
-
-$('form[id^="updatejob-"]').submit(function (event) {
-    event.preventDefault();
-    var data = $( this ).serializeArray();
-    var url = 'http://18.219.63.233:5000/build/';
-    debugger;
-    $.ajax({
-        type: "PUT",
-        url: url,
-        data :  JSON.stringify(data),
-        contentType: "application/json; charset=utf-8",
-        datatype: "json",
-        success:function () {
-            $('div[id^="updateJob-"]').modal('hide');
-            $('body').removeClass('modal-open');
-            $('.modal-backdrop').remove();
-            Swal.fire({
-                type: 'success',
-                title: 'Sucesso!',
-                text: 'Build executado!'
-            });
-
-        },
-        error:function () {
-            Swal.fire({
-                type:'error',
-                title: 'Oops...',
-                text:'Falha ao executar o build!'
-            });
-
-        }
-    })
-
-});
-
 $('form[id^="back-deployment-"]').submit(function (event) {
     event.preventDefault();
     debugger;
@@ -172,33 +170,33 @@ $('form[id^="back-deployment-"]').submit(function (event) {
       cancelButtonColor: '#d33',
       confirmButtonText: 'Sim, desejo!'
     }).then((result) => {
-          if (result.value) {
-              $.ajax({
-                        type: 'POST',
-                        url:'http://18.219.63.233:5000/build/rollback/'+data[0]['value']+'/',
-                        success:function () {
-                            Swal.fire({
-                                type: 'success',
-                                title: 'Sucesso!',
-                                text: 'Rollback executado!',
-                            }).then(function () {
-                                window.location.href= '/deployments';
+        if (result.value) {
+            $.ajax({
+                type: 'POST',
+                url:'http://18.219.63.233:5000/build/rollback/'+data[0]['value']+'/',
+                success:function () {
+                    Swal.fire({
+                        type: 'success',
+                        title: 'Sucesso!',
+                        text: 'Rollback executado!',
+                    }).then(function () {
+                        window.location.href= '/deployments';
 
-                            });
-                        },
-                        error:function () {
-                            Swal.fire({
-                                type:'error',
-                                title: 'Oops...',
-                                text:'Falha ao realizar o rollback'
-                            }).then(function () {
-                                window.location.href= '/deployments';
+                    });
+                },
+                error:function () {
+                    Swal.fire({
+                        type:'error',
+                        title: 'Oops...',
+                        text:'Falha ao realizar o rollback'
+                    }).then(function () {
+                        window.location.href= '/deployments';
 
-                            });
-                            }
+                    });
+                }
 
-    });
-          }
+            });
+        }
     });
     return false;
 });
@@ -216,33 +214,33 @@ $('form[id^="rm-deployment-"]').submit(function (event) {
       cancelButtonColor: '#d33',
       confirmButtonText: 'Sim, desejo remover!'
     }).then((result) => {
-          if (result.value) {
-              $.ajax({
-                        type: 'DELETE',
-                        url:'http://18.219.63.233:5000/build/'+data[0]['value']+'/',
-                        success:function () {
-                            Swal.fire({
-                                type: 'success',
-                                title: 'Sucesso!',
-                                text: 'Remove executado!',
-                            }).then(function () {
-                                window.location.href= '/deployments';
+        if (result.value) {
+            $.ajax({
+                type: 'DELETE',
+                url:'http://18.219.63.233:5000/build/'+data[0]['value']+'/',
+                success:function () {
+                    Swal.fire({
+                        type: 'success',
+                        title: 'Sucesso!',
+                        text: 'Remove executado!',
+                    }).then(function () {
+                        window.location.href= '/deployments';
 
-                            });
-                        },
-                        error:function () {
-                            Swal.fire({
-                                type:'error',
-                                title: 'Oops...',
-                                text:'Falha ao remover'
-                            }).then(function () {
-                                window.location.href= '/deployments';
+                    });
+                },
+                error:function () {
+                    Swal.fire({
+                        type:'error',
+                        title: 'Oops...',
+                        text:'Falha ao remover'
+                    }).then(function () {
+                        window.location.href= '/deployments';
 
-                            });
-                            }
+                    });
+                }
 
-    });
-          }
+            });
+        }
     });
     return false;
 });
@@ -260,39 +258,39 @@ $('form[id^="backup-deployment-"]').submit(function (event) {
       cancelButtonColor: '#d33',
       confirmButtonText: 'Sim, desejo!'
     }).then((result) => {
-          if (result.value) {
-              $.ajax({
-                        type: 'POST',
-                        url:'http://18.219.63.233:5000/build/backup/'+data[0]['value']+'/',
-                        success:function () {
-                            Swal.fire({
-                                type: 'success',
-                                title: 'Sucesso!',
-                                text: 'Backup executado!',
-                            }).then(function () {
-                                window.location.href= '/deployments';
+        if (result.value) {
+            $.ajax({
+                type: 'POST',
+                url:'http://18.219.63.233:5000/build/backup/'+data[0]['value']+'/',
+                success:function () {
+                    Swal.fire({
+                        type: 'success',
+                        title: 'Sucesso!',
+                        text: 'Backup executado!',
+                    }).then(function () {
+                        window.location.href= '/deployments';
 
-                            });
-                        },
-                        error:function () {
-                            Swal.fire({
-                                type:'error',
-                                title: 'Oops...',
-                                text:'Falha ao realizar o Backup'
-                            }).then(function () {
-                                window.location.href= '/deployments';
+                    });
+                },
+                error:function () {
+                    Swal.fire({
+                        type:'error',
+                        title: 'Oops...',
+                        text:'Falha ao realizar o Backup'
+                    })
+                    .then(function () {
+                        window.location.href= '/deployments';
 
-                            });
-                            }
+                    });
+                }
 
-    });
-          }
+            });
+        }
     });
     return false;
 });
 
-
-function showBuildLog(build_id){
+function showBuildLog(build_id) {
 
     if (build_id !== null && build_id !== 0) {
 
@@ -308,4 +306,34 @@ function showBuildLog(build_id){
             }
         })
     }
+}
+
+function setValuesFields(name){
+
+    $('[name="name"]').prop( "disabled", true );
+
+    $.ajax({
+        type: "GET",
+        url: "http://18.219.63.233:5000/build/" + name,
+        datatype: "json",
+        success: function (result) {
+
+            build = result;
+
+            $('#productu').val(result.product);
+            $('#imageu').val(result.image_id);
+            $('[name="name"]').val(result.name);
+            $('[name="cnpj_cpf"]').val(result.cnpj_cpf);
+            $('[name="nome_razaosocial"]').val(result.nome_razaosocial);
+            $('[name="login"]').val(result.login);
+            $('[name="password"]').val(result.password);
+
+            if (result.typedb === "demo")
+                $("#typedbuDEMO").prop("checked", true);
+
+            if (result.typedb === "prod")
+                $("#typedbuPROD").prop("checked", true);
+        
+        }
+    })
 }
